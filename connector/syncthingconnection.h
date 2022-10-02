2abb468d9337bc40b3fac4b28800634ec8158602
Martchus <martchus@gmx.net>
2022-10-02 20:34:40 +0200

    Fix typo

--- connector/syncthingconnection.h
+++ connector/syncthingconnection.h
@@ -56,7 +56,7 @@ enum class SyncthingConnectionLoggingFlags : quint64 {
     None, /**< loggingn is disabled */
     FromEnvironment = (1 << 0), /**< environment variables are checked to pull in any of the other flags dynamically */
     ApiCalls = (1 << 1), /**< log calls to Syncthing's REST-API and responses */
-    ApiReplies = (1 << 2), /**< log replies fromm Syncthing's REST-API */
+    ApiReplies = (1 << 2), /**< log replies from Syncthing's REST-API */
     Events = (1 << 3), /**< log events received via Syncthing's event API */
     DirsOrDevsResetted = (1 << 4), /**< log list of directories/devices when list is reset */
     All = ApiCalls | ApiReplies | Events | DirsOrDevsResetted, /** log as much as possible */
