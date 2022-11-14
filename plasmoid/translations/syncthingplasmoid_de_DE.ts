a30f3d40625fac45320c57b9b896490a6de70662
Martchus <martchus@gmx.net>
2022-11-14 18:41:31 +0100

    Fix typo in German translation

--- plasmoid/translations/syncthingplasmoid_de_DE.ts
+++ plasmoid/translations/syncthingplasmoid_de_DE.ts
@@ -220,7 +220,7 @@
     <message>
         <location filename="../lib/appearanceoptionpage.ui" line="108"/>
         <source>Note that the size is ignored when the Plasmoid is displayed on the desktop, in full screen mode or as entry of the system tray Plasmoid. Then the Plasmoid can be resized using Plasma&apos;s UI. On Plasma older than 5.26, when displayed as entry of the system tray Plasmoid the size can not be changed (setting a high value just leads to the contents being clipped).</source>
-        <translation>Die Größe wird ignoriert wenn das Plasmoid auf dem Desktop, im Vollbildmodus oder als Eintrag des System-Tray-Plasmoids dargestellt wird. Dann kann die Größe direkt über Plasma&apos;s UI angepasst werden. Under Plasma-Versionen älter als 5.26 kann die Größe nicht geändert werden, wenn das Plasmoid innerhalb des Systemtray-Plasmoids dargestellt wird (einen größeren Wert einzustellen führt nur dazu, dass der Inhalt abgeschnitten wird).</translation>
+        <translation>Die Größe wird ignoriert wenn das Plasmoid auf dem Desktop, im Vollbildmodus oder als Eintrag des System-Tray-Plasmoids dargestellt wird. Dann kann die Größe direkt über Plasma&apos;s UI angepasst werden. Unter Plasma-Versionen älter als 5.26 kann die Größe nicht geändert werden, wenn das Plasmoid innerhalb des Systemtray-Plasmoids dargestellt wird (einen größeren Wert einzustellen führt nur dazu, dass der Inhalt abgeschnitten wird).</translation>
     </message>
     <message>
         <location filename="../lib/appearanceoptionpage.ui" line="120"/>
