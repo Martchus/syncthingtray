103b34c64a2b63946fe453d3ee37b46714154bf9
Martchus <martchus@gmx.net>
2023-01-30 16:29:07 +0100

    Apply clang-format

--- connector/syncthingconnection.cpp
+++ connector/syncthingconnection.cpp
@@ -959,7 +959,8 @@ void SyncthingConnection::setStatus(SyncthingStatus status)
 void SyncthingConnection::emitError(const QString &message, const QJsonParseError &jsonError, QNetworkReply *reply, const QByteArray &response)
 {
     if (loggingFlags() & SyncthingConnectionLoggingFlags::ApiReplies) {
-        cerr << Phrases::Error << "JSON parsing error: " << message.toLocal8Bit().data() << jsonError.errorString().toLocal8Bit().data() << " (at offset " << jsonError.offset << ')' << endl;
+        cerr << Phrases::Error << "JSON parsing error: " << message.toLocal8Bit().data() << jsonError.errorString().toLocal8Bit().data()
+             << " (at offset " << jsonError.offset << ')' << endl;
     }
     emit error(message % jsonError.errorString() % QChar(' ') % QChar('(') % tr("at offset %1").arg(jsonError.offset) % QChar(')'),
         SyncthingErrorCategory::Parsing, QNetworkReply::NoError, reply->request(), response);
