7a3607565fbfa71502ae0336ca074d37776e3e66
Martchus <martchus@gmx.net>
2024-11-01 01:29:27 +0100

    Fix duplicate notifications on Android

--- tray/android/src/io/github/martchus/syncthingtray/SyncthingService.java
+++ tray/android/src/io/github/martchus/syncthingtray/SyncthingService.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.util.Log;
 
@@ -17,36 +18,37 @@ public class SyncthingService extends Service
 {
     private final SyncthingServiceBinder m_binder = new SyncthingServiceBinder(this);
     private static final String TAG = "SyncthingService";
+    private static SyncthingService s_instance = null;
     private static final int s_notificationID = 1;
-    private static NotificationManager s_notificationManager;
-    private static NotificationChannel s_notificationChannel;
-    private static Notification.Builder s_notificationBuilder;
-    private static Notification s_notification;
+    private NotificationManager m_notificationManager;
+    private NotificationChannel m_notificationChannel;
+    private Notification.Builder m_notificationBuilder;
+    private Notification m_notification;
     private static String s_notificationTitle = "Syncthing";
     private static String s_notificationText = "Initializing …";
     private static String s_notificationSubText = "";
     private static Bitmap s_notificationIcon = null;
 
     private void initializeNotificationManagement()  {
-        if (s_notificationManager != null) {
+        if (m_notificationManager != null) {
             return;
         }
 
-        s_notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+        m_notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            s_notificationChannel = new NotificationChannel(SyncthingService.class.getCanonicalName(), "Syncthing", NotificationManager.IMPORTANCE_DEFAULT);
-            s_notificationChannel.enableLights(false);
-            s_notificationChannel.enableVibration(false);
-            s_notificationChannel.setSound(null, null);
-            s_notificationChannel.setShowBadge(false);
-            s_notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);
-            s_notificationManager.createNotificationChannel(s_notificationChannel);
-            s_notificationBuilder = new Notification.Builder(this, s_notificationChannel.getId());
+            m_notificationChannel = new NotificationChannel(SyncthingService.class.getCanonicalName(), "Syncthing", NotificationManager.IMPORTANCE_DEFAULT);
+            m_notificationChannel.enableLights(false);
+            m_notificationChannel.enableVibration(false);
+            m_notificationChannel.setSound(null, null);
+            m_notificationChannel.setShowBadge(false);
+            m_notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);
+            m_notificationManager.createNotificationChannel(m_notificationChannel);
+            m_notificationBuilder = new Notification.Builder(this, m_notificationChannel.getId());
         } else {
-            s_notificationBuilder = new Notification.Builder(this);
+            m_notificationBuilder = new Notification.Builder(this);
         }
 
-        s_notificationBuilder
+        m_notificationBuilder
             .setContentTitle(s_notificationTitle)
             .setContentText(s_notificationText)
             .setSubText(s_notificationSubText)
@@ -56,42 +58,50 @@ public class SyncthingService extends Service
             .setOnlyAlertOnce(true)
             .setPriority(Notification.PRIORITY_MIN)
             .setDefaults(Notification.DEFAULT_SOUND)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setGroup(TAG)
+            .setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
             .setAutoCancel(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            m_notificationBuilder.setFlag(Notification.FLAG_NO_CLEAR | Notification.FLAG_FOREGROUND_SERVICE, true);
+        }
     }
 
-    public static void updateNotification(String text, String subText, Bitmap icon) {
+    public static void updateNotification(String text, String subText, Bitmap bitmapIcon) {
         if (text != null) {
             s_notificationText = text;
         }
         if (subText != null) {
             s_notificationSubText = subText;
         }
-        if (icon != null) {
-            s_notificationIcon = icon;
+        if (bitmapIcon != null) {
+            s_notificationIcon = bitmapIcon;
         }
-        if (s_notificationBuilder == null) {
+        if (s_instance == null) {
             return;
         }
         Log.i(TAG, "Updating notification: " + s_notificationText);
-        s_notificationBuilder.setContentText(text).setSubText(subText);
+        s_instance.m_notificationBuilder.setContentText(s_notificationText).setSubText(s_notificationSubText);
         if (s_notificationIcon != null) {
-            s_notificationBuilder.setLargeIcon(icon);
+            Icon icon = Icon.createWithBitmap(s_notificationIcon);
+            s_instance.m_notificationBuilder.setSmallIcon(icon).setLargeIcon(icon);
         }
-        s_notification = s_notificationBuilder.build();
-        s_notificationManager.notify(TAG, s_notificationID, s_notification);
+        s_instance.m_notification = s_instance.m_notificationBuilder.build();
+        s_instance.showForegroundNotification();
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
         initializeNotificationManagement();
+        s_instance = this;
         Log.i(TAG, "Created service and notification");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        s_notificationManager.cancel(s_notificationID);
+        stopForeground(Service.STOP_FOREGROUND_REMOVE);
         Log.i(TAG, "Destroyed service and notification");
     }
 
@@ -104,17 +114,25 @@ public class SyncthingService extends Service
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         Log.i(TAG, "Putting service in foreground and showing notification");
-        if (s_notification == null) {
-            s_notification = s_notificationBuilder.build();
-        }
-        // FIXME: The nofication shown here shows up delayed and thus after updateNotification() is invoked.
-        //        It fortunately doesn't replace the updated notification again but is added as additional
-        //        notification.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(s_notificationID, s_notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
-        } else {
-            startForeground(s_notificationID, s_notification);
-        }
+        showForegroundNotification();
         return Service.START_STICKY;
+    }
+
+    public void showForegroundNotification() {
+        if (m_notification == null) {
+            m_notification = m_notificationBuilder.build();
+        }
+
+        // use startForeground() to make sure the service gets CPU time even when the app is hidden
+        // note: This also shows the notification. Invoking notificationManager.notify() would lead to duplicate notifications.
+        //       We also have to use startForeground() again to update the notification because invoking notificationManager.notify()
+        //       would not update the initial notification and instead again lead to duplicate notifications.
+        //       We also have to use stopForeground() to remove the notification because invoking notificationManager.cancel() doesn't
+        //       have any effect.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(s_notificationID, m_notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+        } else {
+            startForeground(s_notificationID, m_notification);
+        }
     }
 }
