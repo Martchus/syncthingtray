05e159faa4cf96754ee2c5cb3cc1560b326f338f
Martchus <martchus@gmx.net>
2024-12-21 16:42:06 +0100

    Adapt tests of models to latest changes

--- syncthingmodel/tests/models.cpp
+++ syncthingmodel/tests/models.cpp
@@ -85,17 +85,17 @@ void ModelTests::testDirectoryModel()
     QCOMPARE(model.index(1, 0).data(), QStringLiteral("Yet another folder"));
     QCOMPARE(model.index(2, 0).data(), QStringLiteral("A folder which is not shared"));
     const auto folder1Idx = model.index(0, 0);
-    QCOMPARE(model.rowCount(folder1Idx), 10);
+    QCOMPARE(model.rowCount(folder1Idx), 11);
     QCOMPARE(model.index(0, 0, folder1Idx).data(), QStringLiteral("ID"));
     QCOMPARE(model.index(0, 1, folder1Idx).data(), QStringLiteral("GXWxf-3zgnU"));
     QCOMPARE(model.index(1, 0, folder1Idx).data(), QStringLiteral("Path"));
     QCOMPARE(model.index(1, 1, folder1Idx).data(), QStringLiteral("this/path/is/not/supposed/to/exist"));
     const auto folder2Idx = model.index(1, 0);
-    QCOMPARE(model.rowCount(folder2Idx), 10);
+    QCOMPARE(model.rowCount(folder2Idx), 11);
     QCOMPARE(model.index(0, 1, folder2Idx).data(), QStringLiteral("zX8xfl3ygn-"));
     QCOMPARE(model.index(1, 1, folder2Idx).data(), QStringLiteral("..."));
     const auto folder3Idx = model.index(2, 0);
-    QCOMPARE(model.rowCount(folder2Idx), 10);
+    QCOMPARE(model.rowCount(folder2Idx), 11);
     QCOMPARE(model.index(0, 1, folder3Idx).data(), QStringLiteral("forever-alone"));
     QCOMPARE(model.index(1, 1, folder3Idx).data(), QStringLiteral("..."));
 }
@@ -109,17 +109,19 @@ void ModelTests::testDevicesModel()
     QCOMPARE(model.index(1, 0).data(), QStringLiteral("Other instance"));
     QCOMPARE(model.index(1, 1).data(), QStringLiteral("Unknown"));
     const auto dev1Idx = model.index(0, 0);
-    QCOMPARE(model.rowCount(dev1Idx), 5);
+    QCOMPARE(model.rowCount(dev1Idx), 6);
     QCOMPARE(model.index(0, 0, dev1Idx).data(), QStringLiteral("ID"));
     QCOMPARE(model.index(0, 1, dev1Idx).data(), QStringLiteral("P56IOI7-MZJNU2Y-IQGDREY-DM2MGTI-MGL3BXN-PQ6W5BM-TBBZ4TJ-XZWICQ2"));
-    QCOMPARE(model.index(1, 0, dev1Idx).data(), QStringLiteral("Address"));
-    QCOMPARE(model.index(1, 1, dev1Idx).data(), QStringLiteral("dynamic, tcp://192.168.1.2:22000"));
-    QCOMPARE(model.index(4, 0, dev1Idx).data(), QStringLiteral("Introducer"));
-    QCOMPARE(model.index(4, 1, dev1Idx).data(), QStringLiteral("no"));
+    QCOMPARE(model.index(1, 0, dev1Idx).data(), QStringLiteral("Out of Sync items"));
+    QCOMPARE(model.index(1, 1, dev1Idx).data(), QStringLiteral("none"));
+    QCOMPARE(model.index(2, 0, dev1Idx).data(), QStringLiteral("Address"));
+    QCOMPARE(model.index(2, 1, dev1Idx).data(), QStringLiteral("dynamic, tcp://192.168.1.2:22000"));
+    QCOMPARE(model.index(5, 0, dev1Idx).data(), QStringLiteral("Introducer"));
+    QCOMPARE(model.index(5, 1, dev1Idx).data(), QStringLiteral("no"));
     const auto dev2Idx = model.index(1, 0);
-    QCOMPARE(model.rowCount(dev2Idx), 5);
+    QCOMPARE(model.rowCount(dev2Idx), 6);
     QCOMPARE(model.index(0, 1, dev2Idx).data(), QStringLiteral("53STGR7-YBM6FCX-PAZ2RHM-YPY6OEJ-WYHVZO7-PCKQRCK-PZLTP7T"));
-    QCOMPARE(model.index(1, 1, dev2Idx).data(), QStringLiteral("dynamic, tcp://192.168.1.3:22000"));
+    QCOMPARE(model.index(2, 1, dev2Idx).data(), QStringLiteral("dynamic, tcp://192.168.1.3:22000"));
 }
 
 void ModelTests::testFileModel()
