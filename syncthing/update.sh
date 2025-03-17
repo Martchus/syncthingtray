84e8074f9eff759d1cc51a3edc41adb5ed0bd274
Martchus <martchus@gmx.net>
2025-03-17 11:12:59 +0100

    Make script for updating libsyncthing configurable

--- syncthing/update.sh
+++ syncthing/update.sh
@@ -5,7 +5,7 @@ shopt -s nullglob
 
 libsyncthingdir=$(dirname "$0")
 syncthingrepodir=$libsyncthingdir/go/src/github.com/syncthing/syncthing
-special_release_regex='rc|beta|debian'
+special_release_regex=${LIBSYNCTHING_IGNORE_REGEX:-rc|beta|debian}
 latest_tag=
 
 echo '==> Updating Syncthing repo'
@@ -35,11 +35,12 @@ if git -C "$syncthingrepodir" rev-parse --verify "libsyncthing-$latest_tag" > /d
     echo "Nothing to do: A libsyncthing branch \"libsyncthing-$latest_tag\" for the latest release already exists."
     exit 0
 fi
-git -C "$syncthingrepodir" checkout libsyncthing-latest
+latest_branch=libsyncthing-${LIBSYNCTHING_LATEST_SUFFIX:-latest}
+git -C "$syncthingrepodir" checkout "$latest_branch"
 git -C "$syncthingrepodir" rebase "$latest_tag"
 git -C "$syncthingrepodir" branch "libsyncthing-$latest_tag"
 
 echo '==> Pushing updated/new libsyncthing branches to GitHub'
 git -C "$syncthingrepodir" config remote.all.url > /dev/null && remote=all || remote=martchus
-git -C "$syncthingrepodir" push -fu "$remote" libsyncthing-latest:libsyncthing-latest
+git -C "$syncthingrepodir" push -fu "$remote" "$latest_branch:$latest_branch"
 git -C "$syncthingrepodir" push -u "$remote" "libsyncthing-$latest_tag:libsyncthing-$latest_tag"
