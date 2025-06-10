bee26b56c621984fce6954ba632fccf00e943caa
Baikal <g_dms@mail.ru>
2025-06-10 17:08:34 +0800

    Add files via upload

---
+++ fileitemactionplugin/translations/syncthingfileitemaction_ru_RU.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>SyncthingDirActions</name>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="34"/>
+        <source>Status: not available anymore</source>
+        <translation>Статус: больше не доступен</translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="43"/>
+        <source>Folder info for %1</source>
+        <translation>Информация о папке для %1</translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="44"/>
+        <source>Status: </source>
+        <translation>Статус: </translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="73"/>
+        <source>Global: </source>
+        <translation>Глобальный: </translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="74"/>
+        <source>Local: </source>
+        <translation>Локальный: </translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="75"/>
+        <source>Last scan time: </source>
+        <translation>Время последнего сканирования: </translation>
+    </message>
+    <message>
+        <location filename="../syncthingdiractions.cpp" line="76"/>
+        <source>Rescan interval: %1 seconds</source>
+        <translation>Интервал повторного сканирования: %1 секунд</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../syncthingdiractions.cpp" line="81"/>
+        <source>%1 item(s) out-of-sync</source>
+        <translation>
+            <numerusform>%1 элемент не синхронизирован</numerusform>
+            <numerusform>%1 элемента не синхронизировано</numerusform>
+            <numerusform>%1 элементов не синхронизировано</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>SyncthingFileItemAction</name>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="286"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="157"/>
+        <source>Rescan selected items</source>
+        <translation>Повторно сканировать выбранные элементы</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="176"/>
+        <source>Rescan selected folders</source>
+        <translation>Повторно сканировать выбранные папки</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="159"/>
+        <location filename="../syncthingfileitemaction.cpp" line="176"/>
+        <location filename="../syncthingfileitemaction.cpp" line="207"/>
+        <source>Rescan &quot;%1&quot;</source>
+        <translation>Повторное сканирование &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="189"/>
+        <source>Resume selected folders</source>
+        <translation>Возобновить выбранные папки</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="192"/>
+        <source>Pause selected folders</source>
+        <translation>Приостановить выбранные папки</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="207"/>
+        <source>Rescan containing folders</source>
+        <translation>Повторное сканирование содержащих папок</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="219"/>
+        <source>Resume containing folders</source>
+        <translation>Резюме, содержащее папки</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="189"/>
+        <location filename="../syncthingfileitemaction.cpp" line="219"/>
+        <source>Resume &quot;%1&quot;</source>
+        <translation>Резюме &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="223"/>
+        <source>Pause containing folders</source>
+        <translation>Пауза, содержащая папки</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="192"/>
+        <location filename="../syncthingfileitemaction.cpp" line="223"/>
+        <source>Pause &quot;%1&quot;</source>
+        <translation>Пауза &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="247"/>
+        <source>Not a shared directory</source>
+        <translation>Не общий каталог</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemaction.cpp" line="281"/>
+        <source>Select Syncthing config ...</source>
+        <translation>Выберите конфигурацию синхронизации...</translation>
+    </message>
+</context>
+<context>
+    <name>SyncthingFileItemActionStaticData</name>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="84"/>
+        <source>Syncthing connection error</source>
+        <translation>Ошибка соединения Syncthing</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="104"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="112"/>
+        <source>Select Syncthing config file</source>
+        <translation>Выберите файл конфигурации Syncthing</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="133"/>
+        <source>Syncthing config file can not be automatically located</source>
+        <translation>Файл конфигурации Syncthing не может быть найден автоматически</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="140"/>
+        <source>Unable to load Syncthing config from &quot;%1&quot;</source>
+        <translation>Не удалось загрузить конфигурацию Syncthing из &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="149"/>
+        <source>Syncthing config from &quot;%1&quot; does not contain GUI address.</source>
+        <translation>Конфигурация синхронизации из «%1» не содержит адрес графического интерфейса.</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="161"/>
+        <source>Enter API key</source>
+        <translation>Введите ключ API</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="161"/>
+        <source>The selected config file does not contain an API key. Please enter the API key manually:</source>
+        <translation>Выбранный файл конфигурации не содержит API-ключа. Введите API-ключ вручную:</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="163"/>
+        <source>No API key supplied for &quot;%1&quot;.</source>
+        <translation>Для «%1» не предоставлен ключ API.</translation>
+    </message>
+    <message>
+        <location filename="../syncthingfileitemactionstaticdata.cpp" line="122"/>
+        <source>(still using config from &quot;%1&quot;)</source>
+        <translation>(все еще использую конфигурацию из &quot;%1&quot;)</translation>
+    </message>
+</context>
+<context>
+    <name>SyncthingMenuAction</name>
+    <message>
+        <location filename="../syncthingmenuaction.cpp" line="69"/>
+        <source>Syncthing</source>
+        <translation>Syncthing</translation>
+    </message>
+    <message>
+        <location filename="../syncthingmenuaction.cpp" line="80"/>
+        <source>Syncthing - connecting</source>
+        <translation>Syncthing - подключение</translation>
+    </message>
+    <message>
+        <location filename="../syncthingmenuaction.cpp" line="82"/>
+        <source>Syncthing - not connected</source>
+        <translation>Syncthing - не подключено</translation>
+    </message>
+</context>
+</TS>
