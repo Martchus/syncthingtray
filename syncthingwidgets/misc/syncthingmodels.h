d5bd740cb912019870f98128b08dd847180b68cc
Martchus <martchus@gmx.net>
2026-01-20 23:03:32 +0100

    WIP

--- syncthingwidgets/misc/syncthingmodels.h
+++ syncthingwidgets/misc/syncthingmodels.h
@@ -1,6 +1,8 @@
 #ifndef SYNCTHING_DATA_MODELS_H
 #define SYNCTHING_DATA_MODELS_H
 
+#include <syncthingwidgets/misc/syncthinglauncher.h>
+
 #include <syncthingmodel/syncthingdevicemodel.h>
 #include <syncthingmodel/syncthingdirectorymodel.h>
 #include <syncthingmodel/syncthingrecentchangesmodel.h>
@@ -10,6 +12,7 @@
 #include <syncthingconnector/syncthingnotifier.h>
 
 #include <QObject>
+#include <QtVersion>
 
 #ifdef SYNCTHINGWIDGETS_GUI_QTQUICK
 #include <QtQmlIntegration/qqmlintegration.h>
@@ -21,6 +24,8 @@ class SYNCTHINGWIDGETS_EXPORT SyncthingData : public QObject {
     Q_OBJECT
     Q_PROPERTY(Data::SyncthingConnection *connection READ connection CONSTANT)
     Q_PROPERTY(Data::SyncthingNotifier *notifier READ notifier CONSTANT)
+    Q_PROPERTY(QString syncthingVersion READ syncthingVersion CONSTANT)
+    Q_PROPERTY(QString qtVersion READ qtVersion CONSTANT)
 #ifdef SYNCTHINGWIDGETS_GUI_QTQUICK
     QML_ELEMENT
     QML_SINGLETON
@@ -40,6 +45,18 @@ public:
     Data::SyncthingNotifier *notifier()
     {
         return &m_notifier;
+    }
+    QString syncthingVersion() const
+    {
+#ifdef SYNCTHINGWIDGETS_USE_LIBSYNCTHING
+        return Data::SyncthingLauncher::libSyncthingVersionInfo().remove(0, 11);
+#else
+        return tr("not available");
+#endif
+    }
+    QString qtVersion() const
+    {
+        return QString::fromUtf8(qVersion());
     }
 
 private:
@@ -86,6 +103,44 @@ public:
     {
         return &m_changesModel;
     }
+
+    Q_INVOKABLE bool openSyncthingConfigFile();
+    Q_INVOKABLE bool openSyncthingLogFile();
+    Q_INVOKABLE bool openUrlExternally(const QUrl &url, bool viaQt = false);
+    Q_INVOKABLE bool openPath(const QString &path);
+    Q_INVOKABLE bool openPath(const QString &dirId, const QString &relativePath);
+    Q_INVOKABLE bool scanPath(const QString &path);
+    Q_INVOKABLE bool copyText(const QString &text);
+    Q_INVOKABLE bool copyPath(const QString &dirId, const QString &relativePath);
+    Q_INVOKABLE QString getClipboardText() const;
+    Q_INVOKABLE bool loadIgnorePatterns(const QString &dirId, QObject *textArea);
+    Q_INVOKABLE bool saveIgnorePatterns(const QString &dirId, QObject *textArea);
+    Q_INVOKABLE bool openIgnorePatterns(const QString &dirId);
+    Q_INVOKABLE bool loadErrors(QObject *listView);
+    Q_INVOKABLE bool showLog(QObject *textArea);
+    Q_INVOKABLE void clearLog();
+    Q_INVOKABLE bool showQrCode(Icon *icon);
+    Q_INVOKABLE bool loadDirErrors(const QString &dirId, QObject *view);
+    Q_INVOKABLE bool loadStatistics(const QJSValue &callback);
+    Q_INVOKABLE bool showError(const QString &errorMessage);
+    Q_INVOKABLE QString resolveUrl(const QUrl &url);
+    Q_INVOKABLE bool shouldIgnorePermissions(const QString &path);
+    Q_INVOKABLE Data::SyncthingFileModel *createFileModel(const QString &dirId, QObject *parent);
+    Q_INVOKABLE QtGui::DiffHighlighter *createDiffHighlighter(QTextDocument *parent);
+    Q_INVOKABLE QVariantList internalErrors() const;
+    Q_INVOKABLE void clearInternalErrors();
+    Q_INVOKABLE bool postSyncthingConfig(const QJsonObject &rawConfig, const QJSValue &callback = QJSValue());
+    Q_INVOKABLE bool invokeDirAction(const QString &dirId, const QString &action);
+    Q_INVOKABLE bool requestFromSyncthing(
+        const QString &verb, const QString &path, const QVariantMap &parameters, const QJSValue &callback = QJSValue());
+    Q_INVOKABLE QString formatDataSize(quint64 size) const;
+    Q_INVOKABLE QString formatTraffic(quint64 total, double rate) const;
+    Q_INVOKABLE bool hasDevice(const QString &id);
+    Q_INVOKABLE bool hasDir(const QString &id);
+    Q_INVOKABLE QString deviceDisplayName(const QString &id) const;
+    Q_INVOKABLE QString dirDisplayName(const QString &id) const;
+    Q_INVOKABLE QVariantList computeDirsNeedingItems(const QModelIndex &devProxyModelIndex) const;
+    Q_INVOKABLE QVariant isPopulated(const QString &path) const;
 
 private:
     Data::SyncthingDirectoryModel m_dirModel;
